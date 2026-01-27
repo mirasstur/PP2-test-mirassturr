736e9d28763ab60f119cf0cda55e600c9df903c7
mirasstur <mirasstur228@gmail.com>
2026-01-27 13:23:17 +0500

    small update

--- files/01/variables.py
+++ files/01/variables.py
@@ -1,6 +1,11 @@
-a = int(input("Enter number: "))
-b = int(input("Enter second number: "))
-print("Their sum: ", a+b)
+a = 10
+b = 5
+c = a+b
 
 s, t = "it is s", "it is t"
 z = y = "Apple"
+
+print(c)
+print(s)
+print(t)
+print(z,y)
