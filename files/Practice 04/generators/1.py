2ca7f597f9b58d9a9a6b841f3728d2e661e21b91
mirasstur <mirasstur228@gmail.com>
2026-02-24 13:42:53 +0500

    1st task chg

--- files/Practice 04/generators/1.py
+++ files/Practice 04/generators/1.py
@@ -1,3 +1,8 @@
 n = int(input())
 numbers = list(x * x for x in range(1, n + 1))
 print(numbers)
+
+def squares(n):
+    for i in range(n+1):
+        yield (i**2)
+print(" ".join(str(x) for x in squares(n)))
